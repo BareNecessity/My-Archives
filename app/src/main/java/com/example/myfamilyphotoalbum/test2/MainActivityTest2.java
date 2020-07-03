8bc512b88aa859a8287beae30ea84bec6e9dd2e0
dekola <akano.adekola@gmail.com>
2020-07-03 21:50:12 +0100

    Added string for username and password

--- app/src/main/java/com/example/myfamilyphotoalbum/test2/MainActivityTest2.java
+++ app/src/main/java/com/example/myfamilyphotoalbum/test2/MainActivityTest2.java
@@ -3,6 +3,7 @@ package com.example.myfamilyphotoalbum.test2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.myfamilyphotoalbum.R;
 
@@ -12,5 +13,9 @@ public class MainActivityTest2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_test2);
+
+        String userName = ((EditText)findViewById(R.id.editText2)).getText().toString();
+
+        String password = ((EditText)findViewById(R.id.editText)).getText().toString();
     }
 }
