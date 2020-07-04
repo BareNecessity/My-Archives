67bb3e3bc67f44f6c1f2d9143ea8e67ed4369174
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-07-03 19:03:04 -0700

    tosin

--- app/src/main/java/com/example/myfamilyphotoalbum/SecondActivity.java
+++ app/src/main/java/com/example/myfamilyphotoalbum/SecondActivity.java
@@ -30,6 +30,8 @@ public class SecondActivity extends AppCompatActivity {
                 Intent move = new Intent(SecondActivity.this, MainActivity.class);
                 startActivity(move);
 
+                myButton
+
             }
         });
 
