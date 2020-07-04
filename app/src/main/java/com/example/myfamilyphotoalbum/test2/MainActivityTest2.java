de5a5c4c6c4d0389e924c486278c809874959106
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-07-04 09:33:27 -0700

    Added button on lauch page to go to main activity

--- app/src/main/java/com/example/myfamilyphotoalbum/test2/MainActivityTest2.java
+++ app/src/main/java/com/example/myfamilyphotoalbum/test2/MainActivityTest2.java
@@ -2,12 +2,19 @@ package com.example.myfamilyphotoalbum.test2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.myfamilyphotoalbum.R;
+import com.example.myfamilyphotoalbum.test1.MainActivity;
+import com.example.myfamilyphotoalbum.test1.SecondActivity;
 
 public class MainActivityTest2 extends AppCompatActivity {
+    Button myButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,19 @@ public class MainActivityTest2 extends AppCompatActivity {
 
         String password = ((EditText)findViewById(R.id.editText)).getText().toString();
         //Seen. What does it do?
+
+
+        myButton = findViewById(R.id.button7);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(MainActivityTest2.this, MainActivity.class);
+                startActivity(move);
+            }
+        });
+
+
+
+
     }
 }
